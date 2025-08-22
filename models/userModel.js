2988ee2f41a3365d88751fc78f6d1293e4817174
Hassan Kamal AlMosadder <76610671+Hassank455@users.noreply.github.com>
2025-08-22 22:12:15 +0300

    add forget password

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,5 @@
+// crypto is a core module of node we don't need to install
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -15,6 +17,11 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
   password: {
     type: String,
     required: [true, 'User must have a password'],
@@ -33,13 +40,8 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
-  // role: {
-  //   type: String,
-  //   enum: ['user', 'guide', 'lead-guide', 'admin'],
-  //   default: 'user',
-  // },
-  // passwordResetToken: String,
-  // passwordResetExpires: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
   // active: {
   //   type: Boolean,
   //   default: true,
@@ -78,6 +80,26 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   }
   // False means NOT changed
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  // create a new, temporary password for the user using node's crypto module
+  // This creates a 72 characters long, cryptographically strong (very random) password using hexadecimal encoding (numbers 0-9, letters A-F)
+  const resetToken = crypto.randomBytes(32).toString('hex');
+  // We create hashed version of this password using crypto module's createHash function, since we never want to store plain text passwords in the database
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+  // We've chosen "sha256" hashing function, which is a very fast operation (as opposed to bcrypt's slow hashing function), which is why we don't have to do this operation asynchronously, as it takes less than a millisecond to complete. The downside to this is that possible attackers can compare our hash to a list of commonly used passwords a lot more times in a given time frame then if using bcrypt, which is a slow operation. So you can do millions of password checks in the same amount of time that it takes to make 1 check using bcrypt. However, this is not a problem here as:
+  // a) we used a very long and very random password (as opposed to user generated passwords, which usually have meaning and are far from random) and
+  // b) our password is only valid for 10 minutes, so there is literally zero chance for the attacker to guess the password in that short amount of time.
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  // Token valid for 10 minutes
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
---
+++ utils/email.js
@@ -0,0 +1,30 @@
+const nodeMailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  // 1) Create a transporter
+  const transporter = nodeMailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false,
+    logger: true,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+      //  pass: 'pass',
+    },
+  });
+
+  // 2) Define the email options
+  const mailOptions = {
+    from: 'Natours <hassankamal@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
+
+  // 3) Actually send the email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
